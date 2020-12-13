315299244a94d2079c9327566d486025f5465166
Katoinga <evaldiviaci@unsa.edu.pe>
2020-12-13 11:58:32 -0500

    fixes

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ Core* core;
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    //llama al nucleo de la aplicacion o juego
     core = new Core();
     core->show();
     core->displayMainMenu();
